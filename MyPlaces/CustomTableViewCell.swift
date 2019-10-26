45dfbc1404d3aa2163ee9ff655a4c976b4dceabb
ValShm <valboss@mail.ru>
2019-10-26 20:57:11 +0300

    Command View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Валерий Шматок on 26.10.2019.
+//  Copyright © 2019 Валерий Шматок. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+}
